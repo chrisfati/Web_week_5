d55fdb1c246e6f124086f526e5904e08fd253af2
Chrisfati <wanyamachrispinus9@gmail.com>
2025-01-25 19:00:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ let age = 25;                   // Number
 let isStudent = true;           // Boolean
 
 // Output the information in the desired format
-console.log(`Name: ${name} (Type: ${typeof name})`);
-console.log(`Age: ${age} (Type: ${typeof age})`);
-console.log(`Is student: ${isStudent} (Type: ${typeof isStudent})`);
+console.log(`Name: ` + name);
+console.log(`Age: ` + age);
+console.log(`Is student: ` + isStudent);
